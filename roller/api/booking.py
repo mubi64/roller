b5f22244c4793f3f03592bd50b43ef502c0fd525
Mubi <mubashirahmedabbasi64@gmail.com>
2026-01-28 15:33:39 +0500

    Revert "Modify get_or_create_customer to accept default address"
    This reverts commit 9fef4d8d6973d334a8d6ffd8cd95bd960a4f11b0.

--- roller/api/booking.py
+++ roller/api/booking.py
@@ -151,7 +151,7 @@ def make_invoice_from_roller_booking(booking):
 
         # Create or Update
         if customer_id:
-            customer = get_or_create_customer(customer_id, booking.get("name"), settings.default_address)
+            customer = get_or_create_customer(customer_id, booking.get("name"))
         else:
             default_customer = settings.default_customer
             customer = frappe.get_doc("Customer", default_customer)
@@ -452,7 +452,7 @@ def fetch_bookings_from_roller():
         
 
 # Helper: Create/Get Customer
-def get_or_create_customer(customer_id, name, default_address):
+def get_or_create_customer(customer_id, name):
     customer = frappe.db.get_value("Customer", {"custom_roller_customer_id": customer_id})
     if customer:
         return frappe.get_doc("Customer", customer)
@@ -462,7 +462,6 @@ def get_or_create_customer(customer_id, name, default_address):
     doc.customer_type = "Individual"
     doc.customer_group = "All Customer Groups"
     doc.territory = "All Territories"
-	doc.customer_primary_address = default_address
     doc.custom_roller_customer_id = customer_id
     doc.save(ignore_permissions=True)
     return doc
